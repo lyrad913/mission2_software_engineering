b211c8765a0322d80bfeee5ee1ca276530a55077
JunHeeJang <lyradortiz913@gmail.com>
2024-06-12 19:59:57 +0900

    Book 클래스 추

---
+++ misson2/src/Book.java
@@ -0,0 +1,23 @@
+
+public class Book {
+	private int id;
+	private String title;
+	private String author;
+	private int year;
+
+	public Book(int _id, String _title, String _author, int _year) {
+		id = _id;
+		title = _title;
+		author = _author;
+		year = _year;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Book{id: '%d',제목: '%s',저자: '%s',출판년도: %d}", id, title, author, year);
+	}
+
+	public int getId() {
+		return id;
+	}
+}
